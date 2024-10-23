76dbc9f90a3f1f5b38ddd38ab84b1e6dbf1db872
mara-Luc <mara.luciano.ilm@gmail.com>
2024-10-23 14:29:22 -0400

    fixing locations

--- Data_request.php
+++ Data_request.php
@@ -4,12 +4,13 @@
     
     <?php
         // Retrieve submitted information
-        $course_number = htmlspecialchars($_GET["courses"]);
+        $course_number = htmlspecialchars($_GET["users"]);
         $server = "localhost";
         $username = "php";
-        $password = "php_password";
-        $database = "testdb";
+        $password = "Voidnull0";
+        $database = "signinDB";
         $conn = mysqli_connect($server, $username, $password, $database);
+        
         // Check for successful connection
         if (!$conn) {
             die("Connection failed: {mysqli_connect_error()}");
@@ -22,26 +23,11 @@
 
 </head>
 <body>
-    <!-- Original dump of GET and POST
-    <p>GET: <?= var_dump($_GET) ?></p>
-    <p>POST: <?= var_dump($_POST) ?></p>-->
 
-    <!--Refined use of the form action/method
-    <?php
-        // Sanitize inputs
-    //    $firstname = htmlspecialchars($_POST['firstname']);
-    //    $lastname = htmlspecialchars($_POST['lastname']);
-    //    $school = htmlspecialchars($_POST['school']);
-    //    $spell = htmlspecialchars($_POST['spell']);
-
-        // Display the friendly message
-    //    echo "<p>Hello, your name is: $firstname $lastname. Your school is: $school. Your favorite spell is: $spell.</p>";
-    ?>-->
-    You selected course <?= $course_number ?>.<br/>
     <?php
         foreach($result as $row)
         {
-            echo "<option value='{$row['username']}'>{$row['username']}</option>\n";
+            echo "<option value='{$row['username']}'>{$row['name']}</option>\n";
         }
                     
         foreach($result as $row) // There should only be one row returned!
