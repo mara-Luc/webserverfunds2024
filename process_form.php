27b11d6677b6caf94c542f682c0a6431fd013670
mara-Luc <mara.luciano.ilm@gmail.com>
2024-10-26 15:53:58 -0400

    Update process_form

--- process_form.php
+++ process_form.php
@@ -1,35 +1,46 @@
-<?php
-// Retrieve submitted information
-$server = "localhost";
-$username = "php";
-$password = "Voidnull0";
-$database = "signinDB";
-$conn = mysqli_connect($server, $username, $password, $database);
+<!DOCTYPE html>
+<head>
+    <title>Data Request</title>
+    	<?php
+	// Retrieve submitted information
+	$server = "localhost";
+	$username = "php";
+	$password = "Voidnull0";
+	$database = "signinDB";
+	$conn = mysqli_connect($server, $username, $password, $database);
 
-// Check for successful connection
-if (!$conn) {
-    die("Connection failed: {mysqli_connect_error()}");
-}
+	// Check for successful connection
+	if (!$conn) {
+    		die("Connection failed: {mysqli_connect_error()}");
+	}
 
-$sql = "select firstname, lastname, school, spell from courses where username='{$username}';";
-$result = mysqli_query($conn, $sql);
+	$sql = "select firstname, lastname, school, spell from courses where username='{$username}';";
+	$result = mysqli_query($conn, $sql);
+	?>
 
-// Sanitize user inputs
-$firstname = htmlspecialchars($_POST['firstname']);
-$lastname = htmlspecialchars($_POST['lastname']);
-$school = ihtmlspecialchars($_POST['school']);
-$spell = ihtmlspecialchars($_POST['spell']);
+</head>
+<body>
+	<?php
+	// Sanitize user inputs
+	$firstname = htmlspecialchars($_POST['firstname']);
+	$lastname = htmlspecialchars($_POST['lastname']);
+	$school = ihtmlspecialchars($_POST['school']);
+	$spell = ihtmlspecialchars($_POST['spell']);
 
 
-// SQL to insert data
-$sql = "INSERT INTO users (firstname, lastname, school, spell) VALUES ('$firstname', '$lastname', '$school','$spell')";
-$result = mysqli_query($conn, $sql);
+	// SQL to insert data
+	$sql = "INSERT INTO users (firstname, lastname, school, spell) VALUES ('$firstname', '$lastname', '$school','$spell')";
+	$result = mysqli_query($conn, $sql);
 
-// Display the result with HTML formatting
+	// Display the result with HTML formatting
 
-echo $result ? "Success! Data has been added.<br>" : "Failure: " . mysqli_error($conn) . "<br>";
-echo "<p>Hello, your name is: $firstname $lastname. Your school is: $school. Your favorite spell is: $spell.</p>";
+	echo $result ? "Success! Data has been added.<br>" : "Failure: " . mysqli_error($conn) . "<br>";
+	echo "<p>Hello, your name is: $firstname $lastname. Your school is: $school. Your favorite spell is: $spell.</p>";
 
-// Don't forget to close the connection!
-mysqli_close($conn);
-?>
+	// Don't forget to close the connection!
+	mysqli_close($conn);
+	?>
+</body>
+</html>
+
+
