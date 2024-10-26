9908cfe867db1921063648e482795d3c648d9d5f
mara-Luc <mara.luciano.ilm@gmail.com>
2024-10-26 19:30:07 -0400

    update

--- process_form.php
+++ process_form.php
@@ -4,8 +4,21 @@
     <title>Process Form</title>
     
     <?php
-    // Retrieve submitted information
-    $username = htmlspecialchars($_POST["username"]);
+        // Retrieve submitted information
+        $username = htmlspecialchars($_POST["users"]);
+        $server = "localhost";
+        $username = "php";
+        $password = "Voidnull0";
+        $database = "signinDB";
+        $conn = mysqli_connect($server, $username, $password, $database);
+        
+        // Check for successful connection
+        if (!$conn) {
+            die("Connection failed: {mysqli_connect_error()}");
+        }
+
+        $sql = "select firstname, lastname, school, spell from courses where username='{$username}';";
+        $result = mysqli_query($conn, $sql);
     ?>
 
 </head>
@@ -13,6 +26,12 @@
 <body>
     <h1>Process Form</h1>
     <?php
+    // Sanitize inputs
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $lastname = htmlspecialchars($_POST['lastname']);
+    $school = htmlspecialchars($_POST['school']);
+    $spell = htmlspecialchars($_POST['spell']);
+
     foreach ($result as $row) {
         echo "<p>Hello, your name is: " . htmlspecialchars($row['firstname']) . " " . htmlspecialchars($row['lastname']) . ". Your school is: " . htmlspecialchars($row['school']) . ". Your favorite spell is: " . htmlspecialchars($row['spell']) . ".</p>";
     }
