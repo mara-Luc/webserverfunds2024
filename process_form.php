99056c44573d74037c07d861159ef8e65fe04d86
mara-Luc <mara.luciano.ilm@gmail.com>
2024-10-26 20:30:55 -0400

    update

--- process_form.php
+++ process_form.php
@@ -5,7 +5,7 @@
     
     <?php
         // Retrieve submitted information
-        $username = htmlspecialchars($_POST["users"]);
+        $$user_input = htmlspecialchars($_POST["users"]);
         $server = "localhost";
         $username = "php";
         $password = "Voidnull0";
@@ -17,8 +17,19 @@
             die("Connection failed: {mysqli_connect_error()}");
         }
 
-        $sql = "select firstname, lastname, school, spell from users where username='{$username}';";
+        $sql = "SELECT firstname, lastname, school, spell FROM users WHERE username='{$user_input}';";
         $result = mysqli_query($conn, $sql);
+
+        //Seperating the array
+        if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $firstname = htmlspecialchars($row['firstname']);
+            $lastname = htmlspecialchars($row['lastname']);
+            $school = htmlspecialchars($row['school']);
+            $spell = htmlspecialchars($row['spell']);
+        } else {
+            $firstname = $lastname = $school = $spell = "Not found";
+        }
     ?>
 
 </head>
@@ -26,6 +37,7 @@
 <body>
     <h1>Process Form</h1>
     <?php
+
 
     echo "<p>Hello, your name is: $firstname $lastname. Your school is: $school. Your favorite spell is: $spell.</p>";
 
