333b6ebece2206b3e6eb9486de5e1683728c2b7b
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-28 21:58:56 -0500

    updte

--- gpio_pi_bme280.php
+++ gpio_pi_bme280.php
@@ -1,32 +1,51 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
-<head>
-    <title>GPIO</title>
-    <!--Link to my Java Script file with functions-->
-    <script>
-    function toggleLED() 
-    {
-        var xhr = new XMLHttpRequest();
-        
-        xhr.open("POST", "control_led.php", true);
-        
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        
-        xhr.onreadystatechange = function() 
-        {
-            if (xhr.readyState == 4 && xhr.status == 200) 
-            {
-                document.getElementById("status").innerText = xhr.responseText;
-            }
-        };
-        
-        xhr.send("toggle=1");
-    }
-    </script>
-</head>
-<body>
+     <head>
+         <title>GPIO</title>
+          <script>
+           function toggleLED() 
+           { 
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", "control_led.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onload = () => 
+                { 
+                    if (xhr.status === 200) 
+                    { 
+                        document.getElementById("toggleButton").innerText = xhr.responseText;
 
-    <button id="toggleButton" onclick="toggleLED()">Loading...</button>
+                    } 
+                
+                }; 
+                xhr.send("toggle=1");
+            } 
+            
+            function getLEDStatus() 
+            { 
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", "control_led.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onload = () => 
+                    {
+                        if (xhr.status === 200) 
+                        {
+                            document.getElementById("toggleButton").innerText = xhr.responseText;
+                             
+                        } 
+                    };
+                     
+                xhr.send("status=1");
+                 
+            } 
+            
+            window.onload = getLEDStatus;
+         // Check LED status when the page loads 
+         </script>
+          
+        </head>
+        <body>
+            <h1>Control LED</h1>
+            <button id="toggleButton" onclick="toggleLED()">Loading...</button>
     <br><br>
 
     <!--A way back to main site-->
