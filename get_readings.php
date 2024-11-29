2a1f35ca65afe66b40dad8f8bdd9481bc2911e8d
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-28 22:46:32 -0500

    Sensor portion

---
+++ get_readings.php
@@ -0,0 +1,9 @@
+<?php
+$raw = `./bme280`;
+$deserialized = json_decode($raw, true);
+echo json_encode([
+    'temperature' => $deserialized['temperature'],
+    'pressure' => $deserialized['pressure'],
+    'altitude' => $deserialized['altitude']
+]);
+?>
--- gpio_pi_bme280.php
+++ gpio_pi_bme280.php
@@ -2,7 +2,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Toggle LED</title>
+    <title>GPIO Example</title>
+    <!--code for the LED button-->
     <script>
         function toggleLED() {
             const xhr = new XMLHttpRequest();
@@ -30,12 +31,52 @@
 
         window.onload = getLEDStatus; // Check LED status when the page loads
     </script>
+
+    <!--Code for the temp/altude/humid sensor-->
+    <script> 
+    function fetchReadings() 
+    {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "get_readings.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onload = () => 
+        {
+            if (xhr.status === 200) 
+            { 
+                const readings = JSON.parse(xhr.responseText); 
+                document.getElementById("temperature").innerText = readings.temperature; 
+                document.getElementById("pressure").innerText = readings.pressure; 
+                document.getElementById("altitude").innerText = readings.altitude; 
+            }
+                
+        }; 
+        xhr.send(); 
+    } 
+    </script>
+
 </head>
 <body>
-    <button id="toggleButton" onclick="toggleLED()">Loading...</button>
-
+    
+    <h1>GPIO Example</h1>
+    
     <br><br>
+    
+    <button id="toggleButton" onclick="toggleLED()">Loading...</button>
+    
+    <br><br>
+    
+    <h1>Sensor Readings</h1>
+    
+    <button onclick="fetchReadings()">Get Readings</button>
+    
+    <p>Temperature: <span id="temperature">N/A</span></p>
 
+    <p>Pressure: <span id="pressure">N/A</span></p>
+    
+    <p>Altitude: <span id="altitude">N/A</span></p>
+    
+    <br><br>
+    
     <!--A way back to main site-->
     <a href="index.php">Back to the landing page</a>
 
