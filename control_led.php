60bf043925bfe1d14ae9e998480539b3b5a6eb5c
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-28 20:50:40 -0500

    update

--- control_led.php
+++ control_led.php
@@ -1,17 +1,18 @@
 <?php
-if (isset($_POST['toggle'])) {
+if (isset($_POST['toggle']))
+{
     $gpioPin = 23;
     toggleLED($gpioPin);
     echo getLEDStatus($gpioPin);
 }
 
-function toggleLED($pin) {
-    $currentState = shell_exec("gpio -g read $pin");
-    $newState = ($currentState == 0) ? 1 : 0;
-    shell_exec("gpio -g write $pin $newState");
+function toggleLED($pin) 
+{
+    shell_exec("gpio toggle $pin $newState");
 }
 
-function getLEDStatus($pin) {
+function getLEDStatus($pin) 
+{
     $state = shell_exec("gpio -g read $pin");
     return ($state == 1) ? "On" : "Off";
 }
