eb0de4eb8cb318f3d252ed11ce25fe017b6bc459
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-28 21:03:03 -0500

    update

--- gpio_pi_bme280.php
+++ gpio_pi_bme280.php
@@ -3,7 +3,26 @@
 <head>
     <title>GPIO</title>
     <!--Link to my Java Script file with functions-->
-    <script src="Sandbox.js"></script>
+    <script>
+    function toggleLED() 
+    {
+        var xhr = new XMLHttpRequest();
+        
+        xhr.open("POST", "control_led.php", true);
+        
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        
+        xhr.onreadystatechange = function() 
+        {
+            if (xhr.readyState == 4 && xhr.status == 200) 
+            {
+                document.getElementById("status").innerText = xhr.responseText;
+            }
+        };
+        
+        xhr.send("toggle=1");
+    }
+    </script>
 </head>
 <body>
     <h1>Control LED</h1>
