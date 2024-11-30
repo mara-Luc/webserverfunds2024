b7be14acd256edbb2e13803c0a5478d871895fd5
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-30 12:56:42 -0500

    update

--- get_readings.php
+++ get_readings.php
@@ -4,21 +4,9 @@ $command = '/home/MaraLu/raspberry-pi-bme280/bme280';
 
 // Execute the command and capture the output
 $raw = shell_exec($command);
+$deserialized = json_decode($raw);
 
-if ($raw === false) {
-    echo json_encode(['error' => 'Failed to execute sensor command']);
-    exit;
-}
-
-$deserialized = json_decode($raw, true);
-if ($deserialized === null) {
-    echo json_encode(['error' => 'Failed to decode JSON']);
-    exit;
-}
-
-echo json_encode([
     'temperature' => $deserialized['temperature'],
     'pressure' => $deserialized['pressure'],
     'humidity' => $deserialized['humidity']
-]);
 ?>
