67770426334bff4af16b6a1912239c412befebf5
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-29 23:31:11 -0500

    update

--- gpio_pi_bme280.php
+++ gpio_pi_bme280.php
@@ -2,31 +2,6 @@
 <html>
 <head>
     <title>GPIO Example</title>
-    <script>
-        function updateReadings() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "get_sensor_data.php", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                var data = JSON.parse(xhr.responseText);
-                if (data.error) {
-                    console.error(data.error);
-                    return;
-                }
-                document.getElementById("temperature").innerText = data.temperature;
-                document.getElementById("pressure").innerText = data.pressure;
-                document.getElementById("humidity").innerText = data.humidity;
-                document.getElementById("altitude").innerText = data.altitude;
-            } else {
-                console.error("Failed to fetch sensor data");
-            }
-        }
-    };
-    xhr.send();
-}
-
-    </script>
 </head>
 <body>
     <h1>Sensor Readings</h1>
@@ -36,12 +11,32 @@
     <p>Pressure: <span id="pressure">N/A</span></p>
     <p>Humidity: <span id="humidity">N/A</span></p>
     <p>Altitude: <span id="altitude">N/A</span></p>
-    
     <br><br>
-    
     <!--A way back to main site-->
     <a href="index.php">Back to the landing page</a>
-
+    <script>
+        function updateReadings() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "get_sensor_data.php", true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        var data = JSON.parse(xhr.responseText);
+                        if (data.error) {
+                            console.error(data.error);
+                            return;
+                        }
+                        document.getElementById("temperature").innerText = data.temperature;
+                        document.getElementById("pressure").innerText = data.pressure;
+                        document.getElementById("humidity").innerText = data.humidity;
+                        document.getElementById("altitude").innerText = data.altitude;
+                    } else {
+                        console.error("Failed to fetch sensor data");
+                    }
+                }
+            };
+            xhr.send();
+        }
+    </script>
 </body>
 </html>
-
