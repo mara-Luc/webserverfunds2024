6fc8d3e286447f39dbc246e073b49fb266209158
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-30 20:34:42 -0500

    up

--- get_readings.php
+++ get_readings.php
@@ -1,15 +1,6 @@
 <?php
-// Define the path to the bme280 command
-$command = 'sudo /home/MaraLu/raspberry-pi-bme280/bme280';
-
 // Execute the command and capture the output
-$raw = shell_exec($command);
-
-// Check if the command execution was successful
-if ($raw === null) {
-    echo "Error: Unable to execute command.";
-    exit;
-}
+$raw = 'sudo /home/MaraLu/raspberry-pi-bme280/bme280';
 
 // Decode the JSON output
 $deserialized = json_decode($raw, true);
