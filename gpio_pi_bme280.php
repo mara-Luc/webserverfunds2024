0fed81ec9f11193ee46e53a83b5c30f362ff7e21
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-30 23:17:10 -0500

    up

--- gpio_pi_bme280.php
+++ gpio_pi_bme280.php
@@ -17,7 +17,7 @@
     <?php
     if (isset($_POST['submit'])) {
         // Define the path to the bme280 command
-        $command = 'sudo /home/MaraLu/raspberry-pi-bme280/bme280';
+        $command = 'sudo -S /home/MaraLu/raspberry-pi-bme280/bme280';
 
         // Execute the command and capture the output
         $output = shell_exec("$command 2>&1");  // Capture any errors as well
