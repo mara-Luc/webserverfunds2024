6f45615d68354280fdd6658821a17c6af64fc34c
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-30 19:53:25 -0500

    up

--- gpio_pi_bme280.php
+++ gpio_pi_bme280.php
@@ -2,28 +2,16 @@
 <html>
 <head>
     <title>BME280 Sensor Readings</title>
-    <script>
-        function updateReadings() {
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "get_readings.php", true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    document.getElementById("readings").innerHTML = xhr.responseText;
-                }
-            };
-            xhr.send();
-        }
-    </script>
 </head>
 <body>
     <h1>BME280 Sensor Readings</h1>
-    <button onclick="updateReadings()">Update Readings</button>
-    <div id="readings">
-        <p>Temperature: N/A</p>
-        <p>Pressure: N/A</p>
-        <p>Humidity: N/A</p>
-    </div>
+    
+    <form action="get_readings.php" method="POST">
+    <input type="submit" value="Submit"/>   
+    </form>
+    
     <br><br>
+    
     <a href="index.php">Back to the landing page</a>
 </body>
 </html>
