f9c317c40d6e725f79e12cb7aea5e580a323e3a6
mara-Luc <mara.luciano.ilm@gmail.com>
2024-11-30 20:12:50 -0500

    up

--- get_readings.php
+++ get_readings.php
@@ -1,14 +1,11 @@
 <?php
 // Define the path to the bme280 command
-$command = '/home/MaraLu/raspberry-pi-bme280/bme280';
+$command = 'sudo /home/MaraLu/raspberry-pi-bme280/bme280';
 
 // Execute the command and capture the output
 $raw = shell_exec($command);
 
-// Debugging info
-$debug_info = shell_exec("whoami");
-echo "Executing as user: $debug_info <br>";
-
+// Check if the command execution was successful
 if ($raw === null) {
     echo "Error: Unable to execute command.";
     exit;
